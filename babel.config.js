b5a212bdc0c512c2cb3d678d96ab3027906761c5
AGLOP-1354 <jake@jober.io>
2025-08-10 17:23:51 +0900

    [공통] 빌드/린트 베이스 설정 정비 - babel 별칭 정리 및 Reanimated 플러그인 순서 고정 - ESLint v9 flat config 도입(eslint.config.js) - EditorConfig 추가로 에디터간 포맷 일관화 - TSConfig 경로 별칭 유지

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+module.exports = function (api) {
+  api.cache(true)
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      // Path alias plugin
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@': './src',
+            '@/components': './src/components',
+            '@/screens': './src/screens',
+            '@/hooks': './src/hooks',
+            '@/utils': './src/utils',
+            '@/constants': './src/constants',
+            '@/types': './src/types',
+            '@/stores': './src/stores',
+            '@/services': './src/services',
+            '@/navigation': './src/navigation',
+          },
+        },
+      ],
+      // Reanimated plugin (반드시 마지막에 위치)
+      'react-native-reanimated/plugin',
+    ],
+  }
+}
