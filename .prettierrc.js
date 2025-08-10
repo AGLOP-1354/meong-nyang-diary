1e62b501e276ae7aa7871d056900bc83a81b5054
AGLOP-1354 <jake@jober.io>
2025-08-10 17:25:58 +0900

    [공통] Prettier 기본 설정 추가 - 세미콜론 미사용, 싱글쿼트, 트레일링 콤마 등 기본 포맷 규칙

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 120,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+}
