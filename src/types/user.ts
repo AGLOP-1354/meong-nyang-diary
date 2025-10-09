5a124f8666f54cce96a4f5e9bcd4fd526d3faa63
AGLOP-1354 <jake@jober.io>
2025-10-09 16:08:30 +0900

    [타입] TypeScript 타입 정의 추가
    사용자 및 인증 관련 타입 정의 구현

---
+++ src/types/user.ts
@@ -0,0 +1,24 @@
+export type UserType = {
+  id: number
+  uuid: string
+  name: string
+  email: string
+  profileImage: string
+  createdAt: Date
+  updatedAt: Date
+  lastLoginAt: Date
+  version: number
+  kakaoId?: string
+  googleId?: string
+  mobile?: string
+}
+
+export type UserResponseType = {
+  user: UserType
+}
+
+export type AuthState = {
+  isAuthenticated: boolean
+  user: UserType | null
+  isLoading: boolean
+}
